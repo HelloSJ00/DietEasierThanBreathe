711594cac5bb5f25c59bb0400ae930751bd0531b
오승준 <skku03062@g.skku.edu>
2024-06-09 13:42:37 +0900

    final commit

--- js/getMetabolic.js
+++ js/getMetabolic.js
@@ -1,21 +1,24 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  var gender = document.getElementById('gender').value;
-  var weight = parseFloat(document.getElementById('weight').value);
-  var height = parseFloat(document.getElementById('height').value);
-  var age = parseFloat(document.getElementById('age').value);
-  var activity = parseFloat(document.getElementById('activity').value);
+$(document).ready(function () {
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+    var gender = $("#gender").val();
+    var weight = parseFloat($("#weight").val());
+    var height = parseFloat($("#height").val());
+    var age = parseFloat($("#age").val());
+    var activity = parseFloat($("#activity").val());
 
-  var bmr;
-  if (gender === 'male') {
-      bmr = 66 + (13.7 * weight) + (5 * height) - (6.8 * age);
-  } else if (gender === 'female') {
-      bmr = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age);
-  }
+    var bmr;
+    if (gender === "male") {
+      bmr = 66 + 13.7 * weight + 5 * height - 6.8 * age;
+    } else if (gender === "female") {
+      bmr = 655 + 9.6 * weight + 1.8 * height - 4.7 * age;
+    }
 
-  var maintenanceCalories = bmr * activity;
+    var maintenanceCalories = bmr * activity;
 
-  // 결과를 화면에 표시
-  var resultDisplay = document.getElementById('resultDisplay');
-  resultDisplay.innerHTML = '유지 칼로리: ' + maintenanceCalories.toFixed(2) + ' kcal';
+    // 결과를 화면에 표시
+    $("#resultDisplay").html(
+      "유지 칼로리: " + maintenanceCalories.toFixed(2) + " kcal"
+    );
+  });
 });
